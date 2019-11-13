575d9149e2be427256faf7e7676d69d690e22e36
gregwhitaker <gwhitake@gmail.com>
2019-11-12 20:28:06 -0800

    Doing work

--- store-app/src/main/java/acmeshoes/store/controller/PdpController.java
+++ store-app/src/main/java/acmeshoes/store/controller/PdpController.java
@@ -1,8 +1,21 @@
 package acmeshoes.store.controller;
 
+import acmeshoes.store.service.PdpService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PdpController {
 
+    @Autowired
+    private PdpService pdpService;
+
+    @GetMapping("/product/{productId}")
+    public String pdp(@PathVariable("productId") String productId,
+                      Model model) {
+        return null;
+    }
 }
---
+++ store-app/src/main/java/acmeshoes/store/service/PdpService.java
@@ -0,0 +1,8 @@
+package acmeshoes.store.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PdpService {
+
+}
