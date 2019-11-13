1e26d72b5cee4c2c4a1ec4abaae82932dbff380b
gregwhitaker <gwhitake@gmail.com>
2019-11-12 19:03:56 -0800

    Doing work

--- product-service/src/main/java/acmeshoes/service/product/controller/ProductController.java
+++ product-service/src/main/java/acmeshoes/service/product/controller/ProductController.java
@@ -4,9 +4,18 @@ import acmeshoes.service.product.protobuf.ProductInfoRequest;
 import acmeshoes.service.product.protobuf.ProductInfoResponse;
 import acmeshoes.service.product.protobuf.ProductService;
 import io.netty.buffer.ByteBuf;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+/**
+ * Controller responsible for returning product information.
+ */
+@Component
 public class ProductController implements ProductService {
+
+    @Autowired
+    private acmeshoes.service.product.service.ProductService ps;
 
     @Override
     public Mono<ProductInfoResponse> getProductInfo(ProductInfoRequest message, ByteBuf metadata) {
