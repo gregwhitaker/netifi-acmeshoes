a23bd2d920b0ba497ae52aa9cf0a98a71df17cc4
gregwhitaker <gwhitake@gmail.com>
2019-11-12 18:24:35 -0800

    Doing work

--- inventory-service/src/main/java/acmeshoes/service/inventory/controller/InventoryController.java
+++ inventory-service/src/main/java/acmeshoes/service/inventory/controller/InventoryController.java
@@ -18,22 +18,43 @@ package acmeshoes.service.inventory.controller;
 import acmeshoes.service.inventory.protobuf.InventoryService;
 import acmeshoes.service.inventory.protobuf.ProductInventoryRequest;
 import acmeshoes.service.inventory.protobuf.ProductInventoryResponse;
+import acmeshoes.service.inventory.protobuf.SkuInventory;
 import acmeshoes.service.inventory.protobuf.SkuInventoryRequest;
 import acmeshoes.service.inventory.protobuf.SkuInventoryResponse;
 import io.netty.buffer.ByteBuf;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
 public class InventoryController implements InventoryService {
 
+    @Autowired
+    private acmeshoes.service.inventory.service.InventoryService inv;
+
     @Override
     public Mono<ProductInventoryResponse> getProductInventory(ProductInventoryRequest message, ByteBuf metadata) {
-        return null;
+        return inv.getProductInventory(message.getProductId(), message.getShowSoldOut())
+                .collectList()
+                .map(skuInvs -> {
+                    ProductInventoryResponse.Builder piBuilder = ProductInventoryResponse.newBuilder()
+                            .setProductId(message.getProductId());
+
+                    skuInvs.forEach(si -> piBuilder.addSkus(SkuInventory.newBuilder()
+                            .setSku(si.getSku())
+                            .setUnits(si.getUnits())
+                            .build()));
+
+                    return piBuilder.build();
+                });
     }
 
     @Override
     public Mono<SkuInventoryResponse> getSkuInventory(SkuInventoryRequest message, ByteBuf metadata) {
-        return null;
+        return inv.getInventoryForSku(message.getSkuId())
+                .map(skuInv -> SkuInventoryResponse.newBuilder()
+                        .setSku(skuInv.getSku())
+                        .setUnits(skuInv.getUnits())
+                        .build());
     }
 }
